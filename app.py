48dd2e20b7557ae8d9082e309a4d2442c85e1d32
Shirpi <Shirpi@users.noreply.huggingface.co>
2025-12-28 06:51:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,9 @@ def load_db():
     except: pass
 
 def save_db():
-    try: with open(DB_FILE, 'w') as f: json.dump(user_db, f, indent=2)
+    try: 
+    with open(DB_FILE, 'w') as f: 
+        json.dump(user_db, f, indent=2)
     except: pass
 
 load_db()
